8f46ad1c4d788992c7ae306361912fa0a60b1f28
Arijit <arijitdasofficial@gmail.com>
2023-09-10 20:15:03 +0530

    feat: joins

--- 38_left-join.sql
+++ 38_left-join.sql
@@ -7,3 +7,9 @@ SELECT *
 FROM users
   LEFT JOIN cars ON users.car_id = cars.id
 WHERE cars.id IS NULL;
+
+
+-- if pk and fk is same name, we can use USING keyword
+SELECT *
+FROM users
+  LEFT JOIN cars USING (car_id)
